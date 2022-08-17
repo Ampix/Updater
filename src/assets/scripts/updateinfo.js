5fa31f5ce90bcd3790a888a90a879eaef25cac7b
HVCsano <hvcsano924@gmail.com>
2022-08-18 01:13:41 +0200

    1.0.11

--- src/assets/scripts/updateinfo.js
+++ src/assets/scripts/updateinfo.js
@@ -2,11 +2,13 @@ const element = document.getElementById("updateinfo")
 
 const ipcRenderer = require('electron').ipcRenderer;
 const remote = require('electron').remote;
+ipcRenderer.send("giveupdateinfo")
 let updateinfo
 ipcRenderer.on("setupdateinfo", (event, info) => {
       console.log(info)
       ipcRenderer.send("setupdateinfo", info)
 })
+
 ipcRenderer.on("updateinfo", function(event, info) {
       updateinfo = info
       refresh(updateinfo)
