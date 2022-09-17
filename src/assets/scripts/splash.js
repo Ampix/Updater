651b78cf76ca911e90a3f8d3a5b8b2f20b03e96d
HVCsano <hvcsano924@gmail.com>
2022-09-17 12:26:34 +0200

    fix

--- src/assets/scripts/splash.js
+++
@@ -1,4 +0,0 @@
-var element = document.getElementById("updateinfo")
-var progress_bar = document.getElementById("progressparent")
-var progress = document.getElementById("progress")
-const ipcRenderer = require('electron').ipcRenderer;
