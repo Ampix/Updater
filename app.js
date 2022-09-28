7bcd362250bcab1e4e054211db7f325eb1dc3d0f
HVCsano <hvcsano924@gmail.com>
2022-09-28 18:39:45 +0200

    V2

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const {
-  app,
-  BrowserWindow,
-  dialog,
-  ipcMain,
-  ipcRenderer,
-} = require("electron");
+const { app, BrowserWindow, dialog, ipcMain, shell } = require("electron");
+const remoteMain = require("@electron/remote/main");
+remoteMain.initialize();
 const path = require("path");
 const ejse = require("ejs-electron");
 const { autoUpdater } = require("electron-updater");
 const isPackaged = require("electron-is-packaged").isPackaged;
+
 var win;
 var splash;
 var dev = false;
+app.disableHardwareAcceleration();
 
 if (isPackaged === false) {
   dev = true;
@@ -25,36 +23,20 @@ function sleep(ms) {
 
 let dir;
 
-ipcMain.on("selectdirbuilders", function () {
+ipcMain.on("selectdir", function (event, type) {
   dir = dialog
     .showOpenDialog(win, {
-      title: "Ampix Builders Mappaválasztás",
+      title: type + " Mappaválasztás",
       properties: ["openDirectory"],
     })
     .then((result) => {
       if (result.canceled) return;
-      let tipo = "builders";
-      let mappa = {
-        tipo: "builders",
-        folder: result.filePaths[0],
+      let mappa = result.filePaths[0];
+      let stuff = {
+        type: type,
+        folder: mappa,
       };
-      win.webContents.send("selectdirback", (tipo, mappa));
-    });
-});
-ipcMain.on("selectdirt2", function () {
-  dir = dialog
-    .showOpenDialog(win, {
-      title: "Twigmod 2 Mappaválasztás",
-      properties: ["openDirectory"],
-    })
-    .then((result) => {
-      if (result.canceled) return;
-      let tipo = "twigmod2";
-      let mappa = {
-        tipo: "twigmod2",
-        folder: result.filePaths[0],
-      };
-      win.webContents.send("selectdirback", (tipo, mappa));
+      win.webContents.send("selectdirback", stuff);
     });
 });
 
@@ -76,7 +58,7 @@ async function createWindow() {
     show: false,
     icon: "src/assets/icon.png",
     webPreferences: {
-      preload: path.join(app.getAppPath(), "preload.js"),
+      preload: path.join(app.getAppPath(), "/src/assets/scripts/preload.js"),
       nodeIntegration: true,
       contextIsolation: false,
     },
@@ -100,6 +82,7 @@ async function createWindow() {
   splash.center();
   win.loadFile("src/home.ejs");
   win.center();
+  remoteMain.enable(win.webContents);
   if (!dev) {
     win.removeMenu();
     splash.removeMenu();
@@ -118,6 +101,7 @@ app.whenReady().then(async () => {
 function loader() {
   splash.close();
   win.show();
+  //shell.openPath("C:\\Users\\" + username + "\\AppData\\Roaming");
 }
 
 app.on("window-all-closed", () => {
--- src/assets/scripts/window.js
+++ src/assets/scripts/window.js
@@ -1,6 +1,6 @@
 const minus = document.getElementById("minimize");
 const close_app = document.getElementById("close-app");
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, shell, webFrame } = require("electron");
 
 minus.addEventListener("click", minimize);
 close_app.addEventListener("click", closeapp);
